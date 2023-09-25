74bcb266d6aa808e0a3c7a4c104ff69ff2e25f88
Tom Myruski <thomas.myruski@gmail.com>
2023-09-25 09:11:26 -0400

    Remove alerts-old

--- resources/blocks/checkmark-list.php
+++ resources/blocks/checkmark-list.php
@@ -43,25 +43,15 @@ $background = get_field($prefix . 'background');
         </div>
         <div class="checkmark-list__grid <?php echo $layout ? 'checkmark-list__grid--1col' : 'checkmark-list__grid--3col' ?>">
             <?php if (have_rows('items')) : ?>
-
                 <?php while (have_rows('items')) : the_row(); ?>
-
                 <?php
                     $title = get_sub_field('title');
                     $description = get_sub_field('content');
-
                     $svg =  '<svg xmlns="http://www.w3.org/2000/svg" width="33" height="24" viewBox="0 0 33 24" fill="none"> <path d="M10.9311 23.5286L0.471304 13.0688C-0.157101 12.4404 -0.157101 11.4215 0.471304 10.7931L2.747 8.51731C3.37541 7.88884 4.39436 7.88884 5.02277 8.51731L12.069 15.5635L27.1612 0.471304C27.7896 -0.157101 28.8086 -0.157101 29.437 0.471304L31.7127 2.74707C32.3411 3.37547 32.3411 4.39436 31.7127 5.02283L13.2069 23.5287C12.5784 24.1571 11.5595 24.1571 10.9311 23.5286Z"/> </svg>';
                     echo $title ? '<div class="checkmark-list__title">' . $svg . $title . '</div>' : '';
                     echo $description ? '<div class="checkmark-list__description">' . $description . '</div>' : '';
-
-
                 endwhile; ?>
-
             <?php endif; ?>
-
         </div>
-
-
-
     </div>
 </div>
